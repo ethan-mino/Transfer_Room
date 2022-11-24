e400ec173201fe79c9b220b415e9bb4cee404e99
rlfalsgh95 <rlfalsgh95@naver.com>
2022-11-24 10:36:40 +0900

    FIX : 마이페이지 카테고리 디자인 및 js 추가

--- frontend_project/src/components/user/mypage/MyPageVue.vue
+++ frontend_project/src/components/user/mypage/MyPageVue.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="myPageContainer">
+    <div class="content-container">
         <div class="container">
-            <h1 class="container-title">커뮤니티 게시글 내역</h1> <!-- TODO : 모든 글씨가 bold 채라서 게시글 정보가 잘 안보임-->
+            <h1 class="container-title">양도 내역</h1> <!-- TODO : 모든 글씨가 bold 채라서 게시글 정보가 잘 안보임-->
             <div style="display: flex; flex-direction: row; justify-content: center;">
                 <div class="category-list" id="accordian">
                     <ul class="show-dropdown main-navbar">
@@ -9,7 +9,7 @@
                             <div class="top"></div>
                             <div class="bottom"></div>
                         </div>
-                        <li><a >내 정보</a></li>
+                        <li><a>내 정보</a></li>
                         <li><a>1:1 문의</a></li>
                         <li><a>양도 내역</a></li>
                         <li><a>커뮤니티 게시글 내역</a></li>
@@ -53,82 +53,88 @@
 </template>
 
 <script>
+import $ from 'jquery'
+
 export default {
     name: "MyPageVue",
+    created : function() {
 
+    },
     methods: {
-        // activeTab: function (liElem) { 
-        //     $('#accordian ul li').removeClass("active");
-        //     liElem.addClass('active');
+        activeTab: function (liElem) { 
+            $('#accordian ul li').removeClass("active");
+            liElem.addClass('active');
 
-        //     var activeWidthVerticalHeight = liElem.innerHeight();
-        //     var activeWidthVerticalWidth = liElem.innerWidth();
-        //     var itemPosVerticalTop = liElem.position();
-        //     var itemPosVerticalLeft = liElem.position();
+            var activeWidthVerticalHeight = liElem.innerHeight();
+            var activeWidthVerticalWidth = liElem.innerWidth();
+            var itemPosVerticalTop = liElem.position();
+            var itemPosVerticalLeft = liElem.position();
 
-        //     $(".selector-active").css({
-        //         "top":itemPosVerticalTop.top + "px",
-        //         "left":itemPosVerticalLeft.left + "px",
-        //         "height": activeWidthVerticalHeight + "px",
-        //         "width": activeWidthVerticalWidth + "px"
-        //     });
-        // },
-        // test: {
-        //     selectBtn
-        // } 
+            $(".selector-active").css({
+                "top":itemPosVerticalTop.top + "px",
+                "left":itemPosVerticalLeft.left + "px",
+                "height": activeWidthVerticalHeight + "px",
+                "width": activeWidthVerticalWidth + "px"
+            });
+        },
+    },
+    mounted : function (){
+        const firstLiElem = $('#accordian ul li:nth-child(4)')
+        this.activeTab(firstLiElem);
+
+        $(document).on("click", "#accordian ul li", (event) =>{ // 카테고리를 클릭한 경우
+            console.log("asd");
+            const target = event.target;
+            const closestLiElem = $(target).closest("li");
+            
+
+            if(target === closestLiElem[0] || target === closestLiElem.children("a")[0]){  // ! li 태그 또는 자식인 a 태그가 클릭 되었을 때만 동작하도록 해서, 이벤트 위임으로 인해 더보기 버튼(.more-container)을 클릭해도 카테고리가 변경되는 현상을 해결
+                this.activeTab(closestLiElem);   // * a 태그와 가장 가까운 li 태그를 활성화시킴
+            }
+        })
     }
+
 }
 
-// $(document).on("click", "#accordian ul li", (event) =>{ // 카테고리를 클릭한 경우
-//     console.log("asd");
-//     const target = event.target;
-//     const closestLiElem = $(target).closest("li");
-    
 
-//     if(target === closestLiElem[0] || target === closestLiElem.children("a")[0]){  // ! li 태그 또는 자식인 a 태그가 클릭 되었을 때만 동작하도록 해서, 이벤트 위임으로 인해 더보기 버튼(.more-container)을 클릭해도 카테고리가 변경되는 현상을 해결
-//         activeTab(closestLiElem);   // * a 태그와 가장 가까운 li 태그를 활성화시킴
-
-//         const categoryName = closestLiElem.data("label");
-//     }
-// })
 </script>
 
 <style scoped>
 @import "@/assets/css/accordianVerticalTap.css";
 /*@import "https://code.jquery.com/jquery-3.6.1.min.js";*/
 
-    .myPageContainer .container{
+    .container{
         display: flex;
         flex-direction: column;
     }
 
-    .myPageContainer .contents{
+    .contents{
         width : 700px;
-        height: 700px;
+        height: 600px;
         background-color: #ffffff;
         border-radius: 5px;
         box-shadow: 0 0 5px rgba(0,0,0,.2);
         margin-left : 20px;
     }
     
-    .myPageContainer h1.container-title{
+    h1.container-title{
         text-align: center;
-        height: 60px;
+        height: 100px;
         padding-left: 200px;
         padding-top: 10px;
     }
 
-    .myPageContainer .list-table{
+    .list-table{
         border-collapse: collapse;
         width : 100%;
     }
 
-    .myPageContainer .list-table a{
+    .list-table a{
         text-decoration: none;
         color : #000000;
     }
 
-    .myPageContainer .list-table th, .list-table td{
+    .list-table th, .list-table td{
         border-bottom: 1px solid #444444;
         padding: 20px 5px;
     }
